8006cfe1cfd94ede67798f18eb47742b62a0755c
NoobTW <noob@noob.tw>
2017-06-05 19:30:19 +0800

    fix API login mysqli_close

--- api/user/login.php
+++ api/user/login.php
@@ -8,7 +8,7 @@ $pass = $_POST['pass'];
 $sql2 = "SELECT * FROM user WHERE user='$user' AND pass='$pass'";
 $result = mysqli_query($link, $sql2);
 $row = mysqli_fetch_assoc($result);
-
+mysqli_close($link);
 
 if(isset($row)){
 	$data = [
