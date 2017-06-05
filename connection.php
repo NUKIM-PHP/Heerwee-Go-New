76c7ee257458f6db4390a9d128e073cd8bf6cc98
NoobTW <noob@noob.tw>
2017-06-05 20:22:30 +0800

    fix connection.php

--- connection.php
+++ connection.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 session_start();
-$link = mysqli_connect('php-nukim.noob.tw', 'nukim', 'goodtodrink', 'nukim_php_finalterm');
+$link = mysqli_connect('nukim-php.noob.tw', 'nukim', 'goodtodrink', 'nukim_php_finalterm');
 mysqli_query($link, 'SET NAMES UTF8');
