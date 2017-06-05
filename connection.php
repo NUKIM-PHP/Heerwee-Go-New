e6a3f1bb226e9f5359897e93f7cf32e10903f39a
NoobTW <noob@noob.tw>
2017-06-05 20:23:32 +0800

    remove debug info

--- connection.php
+++ connection.php
@@ -1,7 +1,6 @@
 <?php
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 session_start();
 $link = mysqli_connect('nukim-php.noob.tw', 'nukim', 'goodtodrink', 'nukim_php_finalterm');
 mysqli_query($link, 'SET NAMES UTF8');
