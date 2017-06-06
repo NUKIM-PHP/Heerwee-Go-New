cfc815eaaf11be567a2e33542f6e9bb826a2ca1f
NoobTW <noob@noob.tw>
2017-06-07 04:25:25 +0800

    add api delete user

---
+++ api/user/delete.php
@@ -0,0 +1,22 @@
+<?php
+require('../../connection.php');
+header('Content-type: application/json');
+
+$id = $_POST['id'];
+
+if(isset($id)){
+	$sql2 = "DELETE FROM user WHERE id = '$id'";
+	mysqli_query($link, $sql2);
+	mysqli_close($link);
+	$data = [
+		result => 0
+	];
+	echo json_encode($data);
+}else{
+	$data = [
+		result => -98,
+		message => 'Invalid data.'
+	];
+	echo json_encode($data);
+	exit();
+}
