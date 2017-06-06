bed868ae39cbda8777bdf4e97e0683b4e5b7f4bf
asus\user <justtaco0121@gmail.com>
2017-06-06 13:35:20 +0800

    invoice small things

--- api/invoice/getalladmin.php
+++ api/invoice/getalladmin.php
@@ -10,3 +10,13 @@ if(!isset($_SESSION['admin'])){
 	echo json_encode($data);
 	exit();
 }
+
+$sql2 = "SELECT * FROM invoice";
+$result = mysqli_query($link, $sql2);
+$row = mysqli_fetch_assoc($result);
+
+$data = [
+	result => 0
+];
+
+echo json_encode($data);
