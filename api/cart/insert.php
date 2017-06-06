b991e9e298bd4a0b03c89e9a038130ea8aaa2c4e
asus\user <justtaco0121@gmail.com>
2017-06-06 18:39:25 +0800

    fix insert.php

--- api/cart/insert.php
+++ api/cart/insert.php
@@ -1,3 +1,20 @@
+<?php
+require('../../connection.php');
+header('Content-type: application/json');
+
 $u_id = $_SESSION['user'];
+$g_id = $_POST['g_id'];
+$num = $_POST['num'];
 
 u_id VALUES '$u_id';
+
+$sql2 = "INSERT INTO goods('u_id', 'g_id', 'num') VALUES('$u_id', '$g_id', '$num')";
+$result = mysqli_query($link, $sql2);
+$row = mysqli_fetch_assoc($result);
+
+$data = [
+	result => 0
+];
+
+header('Content-type: application/json');
+echo json_encode($data);
