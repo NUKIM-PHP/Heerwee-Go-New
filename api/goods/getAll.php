5ef822d87137ee83a13e631edd66ebb8ceaed266
NoobTW <noob@noob.tw>
2017-06-07 04:15:07 +0800

    fuck the session

--- api/goods/getAll.php
+++ api/goods/getAll.php
@@ -2,15 +2,6 @@
 require('../../connection.php');
 header('Content-type: application/json');
 
-if(!isset($_SESSION['admin'])){
-	$data = [
-		result => -99,
-		message => 'You are not an admin.'
-	];
-	echo json_encode($data);
-	exit();
-}
-
 $sql2 = "SELECT * FROM `goods`";
 $result = mysqli_query($link, $sql2);
 $goods = [];
