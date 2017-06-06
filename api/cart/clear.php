96caed5591ec9c62a14d6ab2fa1a33797ec475ac
asus\user <justtaco0121@gmail.com>
2017-06-06 18:38:33 +0800

    fix clear.php

--- api/cart/clear.php
+++ api/cart/clear.php
@@ -1,3 +1,31 @@
+<?php
+require('../../connection.php');
+header('Content-type: application/json');
+
 $u_id = $_SESSION['user'];
 
-DELETE WHERE u_id = '$u_id';
+if(!isset($_SESSION['user'])){
+	$data = [
+		result => -99,
+		message => 'Need to login.'
+	];
+	echo json_encode($data);
+	exit();
+}
+
+if(isset($_SESSION['user'])){
+	$sql2 = "DELETE FROM cart WHERE u_id = '$u_id'";
+	mysqli_query($link, $sql2);
+	mysqli_close($link);
+	$data = [
+		result => 0
+	];
+	echo json_encode($data);
+}else{
+	$data = [
+	result => -98,
+	message => 'Invalid data.'
+];
+	echo json_encode($data);
+	exit();
+}
