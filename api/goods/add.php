be372ac3ca954628cb79f1d7911daa6bc3c6433d
asus\user <justtaco0121@gmail.com>
2017-06-06 19:50:43 +0800

    no message

--- api/goods/add.php
+++ api/goods/add.php
@@ -23,7 +23,7 @@ if(
 
 $sql2 = "INSERT INTO goods(name, price, cat_id, invnum) VALUES ('$name', '$price', '$cat_id', '$invnum') ";
 $result = mysqli_query($link, $sql2);
-$row = mysqli_fetch_assoc($result);
+mysqli_close($link);
 
 $data = [
 	result => 0
