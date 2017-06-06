325b41ed824b49ac1c8c88a8fa88a4803390e4cd
NoobTW <noob@noob.tw>
2017-06-07 02:35:03 +0800

    fix session

--- connection.php
+++ connection.php
@@ -1,4 +1,6 @@
 <?php
-session_start();
+if (session_status() == PHP_SESSION_NONE) {
+	session_start();
+}
 $link = mysqli_connect('nukim-php.noob.tw', 'nukim', 'goodtodrink', 'nukim_php_finalterm');
 mysqli_query($link, 'SET NAMES UTF8');
