986289545c9d641a583ad72246dbd5f71191228d
NoobTW <noob@noob.tw>
2017-06-07 08:29:13 +0800

    fix product index

--- index.php
+++ index.php
@@ -20,18 +20,18 @@
 			<div class="products-home">
 				<div class="products-home-container">
 					<div class="product-home product animated fadeIn" id="product-home-1">
-						<img src="img/<?= $data->events[0]->pic; ?>" alt="">
+						<img src="img/product1.jpg" alt="">
 					</div>
 					<div class="product-home product animated fadeIn" id="product-home-2">
-						<img src="img/<?= $data->events[2]->pic; ?>" alt="">
+						<img src="img/product3.jpg" alt="">
 					</div>
 				</div>
 				<div class="products-home-container">
 					<div class="product-home product animated fadeIn" id="product-home-3">
-						<img src="img/<?= $data->events[1]->pic; ?>" alt="">
+						<img src="img/product2.jpg" alt="">
 					</div>
 					<div class="product-home product animated fadeIn" id="product-home-4">
-						<img src="img/<?= $data->events[3]->pic; ?>" alt="">
+						<img src="img/product4.jpg" alt="">
 					</div>
 				</div>
 			</div>
