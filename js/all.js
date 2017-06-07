380c0db585636e39896ed2988da86a5bec390951
NoobTW <noob@noob.tw>
2017-06-07 11:46:45 +0800

    efef

--- js/all.js
+++ js/all.js
@@ -2,9 +2,7 @@
 function initSlider(){
 	// var jsImg = ['img/event_1.png', 'img/event_2.png', 'img/event_3.png', 'img/event_4.png'];
 	var jsImg_len;
-	if(jsImg){
-		jsImg_len = jsImg.length;
-	}
+	jsImg_len = jsImg.length;
 	var i=0;
 	$('#events').attr('src', jsImg[i]);
 	i++;
